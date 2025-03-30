47d208206282e28c101e3df29f5319f04300cd98
aditya3329-tech <adityash3328@gmail.com>
2025-03-30 20:23:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function loadNewPage() {
+    window.location.href = 'payment.html'; // Replace with your file name
+}
+function loadPage() {
+    window.location.href = 'index.html'; // Replace with your file name
+}
+function insta() {
+    window.location.href = 'https://www.instagram.com/aditya_sharma._.01?igsh=MTN2OWY0NGp4cGEyeA=='; // Replace with your file name
+}
+function linkedin() {
+    window.location.href = 'https://www.linkedin.com/in/aditya-sharma-a14a27332?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'; // Replace with your file name
+}
